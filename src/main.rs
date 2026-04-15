d007054113ee30eab395b14f1eb21332f8f9c449
Noah <noah.gosciniak1@gmail.com>
2026-04-15 17:35:05 +0200

    add: argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+
+fn print_help()
+{
+    println!("Usage: ./raytracer <SCENE_FILE>\n\tSCENE_FILE: scene configuration");
+}
+
+fn parse_args() -> Option<String>
+{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        print_help();
+        process::exit(1);
+    }
+    let argument: &str = &args[1];
+    match argument {
+        "-h" | "--help" => {
+            print_help();
+            None
+        },
+        _ => { Some(argument.to_string()) }
+    }
+}
+
+
+fn main() -> std::io::Result<()>
+{
+    if let Some(argument) = parse_args() {
+        println!("{}", argument);
+        Ok(())
+    } else {
+        Ok(())
+    }
 }
